6888fe94b0fcb634f8423fc09f3f4db28f103ca5
freitagb <freitagb@nsstc.uah.edu>
2020-03-09 19:05:36 +0000

    removed unused code in update credentials

--- code/update_credentials.py
+++ code/update_credentials.py
@@ -13,8 +13,4 @@ def assume_role(role_arn,role_session_name):
     '''
     client = boto3.client('sts')
     creds = client.assume_role(RoleArn=role_arn, RoleSessionName=role_session_name)
-    access_key_id = creds.get('Credentials').get('AccessKeyId')
-    secret_access_key = creds.get('Credentials').get('SecretAccessKey')
-    session_token = creds.get('Credentials').get('SessionToken')
-
     return creds['Credentials']
