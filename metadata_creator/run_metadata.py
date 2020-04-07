ff74cfde2c0ed02c422e598e3c8f6853441eecc0
EC2 Default User <ec2-user@ip-172-31-21-82.us-west-2.compute.internal>
2020-04-07 16:53:52 +0000

    cleaned up run_metadata for debugging

--- metadata_creator/run_metadata.py
+++ metadata_creator/run_metadata.py
@@ -3,6 +3,7 @@ import boto3
 import datetime
 import click
 import update_credentials
+import numpy as np
 
 from metadata_creator import Metadata
 
@@ -16,21 +17,45 @@ credentials and the s3 resource every 15 minutes. Note that the script
 ignores the hdr file and only accesses the hdf files in the data bucket.
 """
 
-bucket_name = "hls-global"
-product_id = "L30"
-folder = "data"
-path = "/".join([product_id, folder, ""])
+file_type = "debug"
+s2Tile = "T54HTD"
+
+buckets = {"sample": "hls-global",
+    "debug": "hls-debug-output"
+}
+
+bucket_name = buckets[file_type]
+
+paths = {"sample": ["S30/data/HLS.S30.17MPP"],
+    "debug": [
+        "HLS.S30.T06WWA",
+        "HLS.S30.T12RTS",
+        "HLS.S30.T54HTD",
+        "HLS.S30.T55MCP",
+        "HLS.S30.T55MEP",
+        "HLS.S30.T60UXF",
+        "HLS.S30.T60UXG",
+        "HLS.S30.T60WVC",
+    ]
+}
+
+index = np.argwhere(np.char.find(np.asarray(paths[file_type]),s2Tile) >= 0)[0][0]
+path = paths[file_type][index]
+print(path)
 
 creds = update_credentials.assume_role(
     "arn:aws:iam::611670965994:role/gcc-S3Test", "brian_test"
 )
 
-s3 = boto3.resource(
-    "s3",
-    aws_access_key_id=creds["AccessKeyId"],
-    aws_secret_access_key=creds["SecretAccessKey"],
-    aws_session_token=creds["SessionToken"],
-)
+if file_type == "sample":
+    s3 = boto3.resource(
+        "s3",
+        aws_access_key_id=creds["AccessKeyId"],
+        aws_secret_access_key=creds["SecretAccessKey"],
+        aws_session_token=creds["SessionToken"],
+    )
+if file_type == "debug":
+    s3 = boto3.resource("s3")
 bucket = s3.Bucket(bucket_name)
 
 
@@ -57,16 +82,12 @@ def run_metadata():
             reset_credentials = True
 
         file_name = obj.key.split("/")[-1]
-        if file_name.endswith("hdf") and not os.path.exists(
-            "output/" + product_id + "/" + file_name.replace("hdf", "cmr.xml")
-        ):
+        if file_name.endswith(".v1.5.hdf"):
             bucket.download_file(obj.key, file_name)
             result = Metadata(file_name).save_to_S3()
             count += 1
             print(count, ": ", file_name, " - ", result)
             os.remove(file_name)
-        elif file_name.endswith("hdf"):
-            count += 1
         else:
             pass
 
