e91ad93c33eb46409e7571b75724147068f012e0
Chris Holden <ceholden@gmail.com>
2026-05-26 15:48:51 -0400

    chore: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="metadata_creator",
-    version="2.8",
+    version="2.9",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
